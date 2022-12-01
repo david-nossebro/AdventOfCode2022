bd199d974ed0f2e6df47185a587aaae2d7cc9f3d
David Berg <david.nossebro@gmail.com>
2022-12-01 18:23:32 +0100

    Prepared for day 2 and added util to read from an API.

---
+++ src/main/kotlin/day2/Day2.kt
@@ -0,0 +1,10 @@
+package day2
+
+import utils.readFile
+import utils.readUrl
+
+fun main() {
+    //val input = readFile("day2.txt")
+    val input = readUrl("https://google.se")
+    println(input)
+}
--- src/main/kotlin/utils/CommonUtils.kt
+++ src/main/kotlin/utils/CommonUtils.kt
@@ -1,6 +1,8 @@
 package utils
 
 import java.io.File
+import java.net.HttpURLConnection
+import java.net.URL
 
 val NEW_LINE: String = System.lineSeparator()
 val BLANC_LINE = NEW_LINE + NEW_LINE
@@ -11,3 +13,34 @@ fun List<String>.sum() = sumOf { it.toInt() }
 
 fun readFile(fileName: String)
         = File("src/main/resources/inputs/$fileName").inputStream().readBytes().toString(Charsets.UTF_8)
+
+fun readUrl(url: String) = sendRequest(url).body
+
+/**
+ * Sends an HTTP request to the given [url], using the given HTTP [method]. The request can also
+ * include custom [headers] and [body].
+ *
+ * Returns the [Response] object containing [statusCode][Response.statusCode],
+ * [headers][Response.headers] and [body][Response.body].
+ */
+fun sendRequest(url: String, method: String = "GET", headers: Map<String, String>? = null, body: String? = null): Response {
+    val conn = URL(url).openConnection() as HttpURLConnection
+
+    with(conn) {
+        requestMethod = method
+        doOutput = body != null
+        headers?.forEach(this::setRequestProperty)
+    }
+
+    if (body != null) {
+        conn.outputStream.use {
+            it.write(body.toByteArray())
+        }
+    }
+
+    val responseBody = conn.inputStream.use { it.readBytes() }.toString(Charsets.UTF_8)
+
+    return Response(conn.responseCode, conn.headerFields, responseBody)
+}
+
+data class Response(val statusCode: Int, val headers: Map<String, List<String>>? = null, val body: String? = null)
