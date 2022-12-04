f5a0dc624dbb96b190ff4dfddd47a9a0f63c3730
David Berg <david.nossebro@gmail.com>
2022-12-04 11:23:12 +0100

    Day 4 added

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,21 +1,34 @@
 package day4
 
 import utils.readFile
+import utils.splitOnNewLine
 
 fun main() {
     val input = readFile("day4.txt")
-    println("Input: $input")
 
     println("P1: ${part1(input)}")
     println("P2: ${part2(input)}")
 }
 
-fun part1(input: String): String {
-
-    return ""
+fun part1(input: String): Int {
+    return input.splitOnNewLine().map { row ->
+        row.split(",").map { elfRangeString ->
+            elfRangeString.split("-").map { it.toInt() }
+        }.map { (areaStart, areaEnd) -> (areaStart..areaEnd) }
+    }.count { (firstElfArea, secondElfArea) ->
+        firstElfArea containsAllOrIsContainedIn secondElfArea
+    }
 }
 
-fun part2(input: String): String {
-
-    return ""
+fun part2(input: String): Int {
+    return input.splitOnNewLine().map { row ->
+        row.split(",").map { elfRangeString ->
+            elfRangeString.split("-").map { it.toInt() }
+        }.map { (areaStart, areaEnd) -> (areaStart..areaEnd) }
+    }.count { (firstElfArea, secondElfArea) -> firstElfArea overlaps secondElfArea }
 }
+
+infix fun IntRange.overlaps(otherRange: IntRange) = (this intersect otherRange).isNotEmpty()
+
+infix fun IntRange.containsAllOrIsContainedIn(otherRange: IntRange) =
+    otherRange.all { this.contains(it) } || this.all { otherRange.contains(it) }
