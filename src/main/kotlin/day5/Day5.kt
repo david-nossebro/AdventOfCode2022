dd2660e7a187ac3a73c032b4b20962e40f4d9959
David Berg <david.nossebro@gmail.com>
2022-12-05 14:36:36 +0100

    Added day 5

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -1,21 +1,70 @@
 package day5
 
 import utils.readFile
+import utils.splitOnBlancLine
 import utils.splitOnNewLine
 
 fun main() {
-    val inputRows = readFile("day5.txt").splitOnNewLine()
+    val input = readFile("day5.txt")
 
-    println("P1: ${part1(inputRows)}")
-    //println("P2: ${part2(inputRows)}")
+    println("Part 1: ${part1(input)}") //TDCHVHJTG
+    println("Part 2: ${part2(input)}") //NGCMPJLHV
 }
 
-fun part1(inputRows: List<String>): Int {
+data class Instruction(val nrOfItems: Int, val from: Int, val to: Int)
 
-    return 27
+fun String.toStacks() = splitOnBlancLine().first().splitOnNewLine().dropLast(1).map {
+        it.chunked(4).map { it.trim() }
+    }.fold(
+        emptyMap<Int, ArrayDeque<Char>>().toMutableMap()
+    ) {
+        acc, strings ->
+        strings.forEachIndexed {
+                index, item ->
+            if(item.isNotEmpty()) {
+                when {
+                    acc.containsKey(index + 1) -> acc[index + 1]!!.addLast(item[1])
+                    else -> acc[index + 1] = ArrayDeque(listOf(item[1]))
+                }
+            }
+        }
+        acc
+    }
+
+fun String.toInstructions() = splitOnBlancLine()[1].splitOnNewLine().map {
+    val (nrOfItems, from, to) = it.split("move ", " from ", " to ").drop(1).map{ it.toInt() }
+    Instruction(nrOfItems, from, to)
 }
 
-fun part2(inputRows: List<String>): Int {
 
-    return 27
+
+fun part1(input: String): String {
+
+    val stacks = input.toStacks()
+    val instructions = input.toInstructions()
+
+    instructions.forEach { instruction ->
+        repeat(instruction.nrOfItems) {
+            stacks[instruction.to]?.addFirst(
+                stacks[instruction.from]?.removeFirst() ?: error("Stack not found ${instruction.from}")
+            ) ?: error("Stack not found ${instruction.to}")
+        }
+    }
+
+    return stacks.toSortedMap().map { it.value.firstOrNull() ?: "" }.joinToString("")
+}
+
+fun ArrayDeque<Char>.addAllFirst(items: List<Char>) = items.reversed().forEach { addFirst(it) }
+fun ArrayDeque<Char>.removeAllFirst(nrOfItems: Int) = (0..nrOfItems).map { this.removeFirst() }
+fun part2(input: String): String {
+    val stacks = input.toStacks()
+    val instructions = input.toInstructions()
+
+    instructions.forEach { instruction ->
+        val itemsToBeMoved = stacks[instruction.from]?.removeAllFirst(instruction.nrOfItems - 1) ?:
+            error("Stack not found: ${instruction.from}")
+        stacks[instruction.to]?.addAllFirst(itemsToBeMoved) ?: error("Stack not found ${instruction.to}")
+    }
+
+    return stacks.toSortedMap().map { it.value.firstOrNull() ?: "" }.joinToString("")
 }
