d56da855e5a8c977bbfa5e35c8fe033694d76f6b
David Berg <david.berg@sbab.se>
2022-12-06 06:54:38 +0100

    Some refactoring

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -3,17 +3,19 @@ package day6
 import utils.readFile
 
 fun main() {
-    require(part1(readFile("day6-test.txt")) == "7")
+    require(part1(readFile("day6-test.txt")) == 7)
     println("P1: ${part1(readFile("day6.txt"))}")
 
-    require(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == "19")
-    require(part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == "23")
-    require(part2("nppdvjthqldpwncqszvftbrmjlhg") == "23")
-    require(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == "29")
-    require(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == "26")
+    require(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19)
+    require(part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23)
+    require(part2("nppdvjthqldpwncqszvftbrmjlhg") == 23)
+    require(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
+    require(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26)
     println("P2:  ${part2(readFile("day6.txt"))}")
 }
 
-fun part1(input: String) = input.windowed(4).indexOfFirst { it.toSet().size == 4 }.let { it + 4 }.toString()
+fun part1(input: String) = input.findMarkerIndex(4)
+fun part2(input: String) = input.findMarkerIndex(14)
 
-fun part2(input: String): String = input.windowed(14).indexOfFirst { it.toSet().size == 14 }.let { it + 14 }.toString()
+private fun String.findMarkerIndex(wSize: Int) =
+    windowed(wSize).indexOfFirst { it.toSet().size == wSize } + wSize
